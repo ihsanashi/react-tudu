8c7dc02debd2b74e2b7bd389a630750aa264cb80
Ahmad Ihsan <ihsanashi@gmail.com>
2021-05-31 19:08:24 +0800

    remove container div, adjust hview and delete

--- src/components/List.js
+++ src/components/List.js
@@ -1,46 +1,48 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { TodoContext } from '../contexts/TodoContext';
-import { HiOutlineTrash } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
 function List() {
   const { todos, dispatch } = useContext(TodoContext);
 
+  useEffect(() => {}, [todos]);
+
   return (
-    <div className='container'>
-      <ul className='max-w-xl mx-auto'>
-        {todos.map((item) => (
-          <li
-            key={item.id}
-            className='flex flex-row items-center justify-between my-3 bg-gray-100 p-5 rounded-lg'
-          >
-            <div className='flex flex-row items-center justify-start'>
-              <input
-                className='rounded bg-gray-300 border-transparent focus:border-transparent focus:bg-gray-300 text-blue-700 focus:ring-1 focus:ring-offset-2 focus:ring-blue-500'
-                type='checkbox'
-                value={item.completed}
-                onChange={() => !item.completed}
-              />
-              <Link
-                to={`/todo/${item.title.replace(/\s+/g, '-').toLowerCase()}`}
-                id={item.id}
-                className='ml-4'
-                htmlFor={item.title}
-              >
-                {item.title}
-              </Link>
-            </div>
+    <ul className='max-w-xl mx-auto'>
+      {todos.map((item) => (
+        <li
+          key={item.id}
+          className='flex flex-row items-center justify-between my-3 bg-gray-100 p-5 rounded-lg'
+        >
+          <div className='flex flex-row items-center justify-start'>
+            <input
+              className='rounded bg-gray-300 border-transparent focus:border-transparent focus:bg-gray-300 text-blue-700 focus:ring-1 focus:ring-offset-2 focus:ring-blue-500'
+              type='checkbox'
+              value={item.completed}
+              onChange={() => !item.completed}
+            />
+            <p className='text-gray-800 text-base ml-4'>{item.title}</p>
+          </div>
+          <div className='flex flex-row items-center'>
+            <Link
+              to={`/todo/${item.title.replace(/\s+/g, '-').toLowerCase()}`}
+              id={item.id}
+              className='mr-3 text-sm text-blue-500 font-medium'
+              htmlFor={item.title}
+            >
+              View
+            </Link>
             <button
               type='button'
-              className='text-red-500'
+              className='text-red-500 text-sm font-medium'
               onClick={() => dispatch({ type: 'REMOVE_TODO', id: item.id })}
             >
-              <HiOutlineTrash size='20' title='Delete to-do item' />
+              Delete
             </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
