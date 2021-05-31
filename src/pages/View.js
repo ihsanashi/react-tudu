27a9febc1d1a819efba5d2208ab1412d092c655c
Ahmad Ihsan <ihsanashi@gmail.com>
2021-05-31 19:11:43 +0800

    allow existing inputs to be editable, but yet to implement saving existing to-do item

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,37 +1,146 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Helmet } from 'react-helmet';
-import Wrapper from '../components/Wrapper';
-import BackHeader from '../components/BackHeader';
+import { Link, Route, useHistory } from 'react-router-dom';
+import { HiOutlineTrash, HiOutlineChevronLeft } from 'react-icons/hi';
 import { TodoContext } from '../contexts/TodoContext';
-import { Route } from 'react-router-dom';
+import Wrapper from '../components/Wrapper';
+import Banner from '../components/Banner';
 import NoMatch from './NoMatch';
+import Header from '../components/Header';
 
 function ViewTodo(props) {
-  const { todos } = useContext(TodoContext);
+  // Determine if the current URL matches with a todo item title from the todos array
+  const { todos, dispatch } = useContext(TodoContext);
+  const itemTitle = props.match.params.title.replace(/-/g, ' ');
+  const item = todos.find((item) => item.title === itemTitle);
 
-  // Determine if the current URL matches with a todo title from the todos array
-  const itemTitle = props.match.params.title.replace(/-/g, ' ').toLowerCase();
-  const pageExists = todos.some(
-    (item) => item.title.toLowerCase() === itemTitle
-  );
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-  const itemTitleCapitalized = itemTitle.replace(
-    /(^\w{1})|(\s{1}\w{1})/g,
-    (match) => match.toUpperCase()
-  );
+  // initialise input states with existing values
+  const [title, setTitle] = useState(item && item.title);
+  const [description, setDescription] = useState(item && item.description);
 
-  if (pageExists) {
+  const history = useHistory();
+
+  const redirectToHome = () => {
+    const path = '/';
+    history.push(path);
+  };
+
+  const deleteItem = () => {
+    dispatch({ type: 'REMOVE_TODO', id: item.id });
+    redirectToHome();
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    dispatch({
+      type: 'SAVE_TODO',
+      item: {
+        title: title.toLowerCase(),
+        description,
+        id: item.id,
+        completed: item.completed,
+      },
+    });
+
+    setTimeout(() => {
+      setSubmitted(true);
+      setTimeout(() => {
+        setSubmitted(false);
+      }, 5000);
+      setLoading(false);
+    }, 3000);
+  };
+
+  if (item) {
     return (
       <>
         <Helmet>
           <meta name='description' content='View to-do item' />
-          <title>Tudu - {itemTitleCapitalized}</title>
+          <title>
+            Tudu -{' '}
+            {item.title.replace(/(^\w{1})|(\s{1}\w{1})/g, (match) =>
+              match.toUpperCase()
+            )}
+          </title>
         </Helmet>
-        <BackHeader link='/' />
+        <Header
+          left={
+            <Link to='/'>
+              <button className='flex flex-row justify-between items-center'>
+                <HiOutlineChevronLeft size='24' className='text-gray-100' />
+                <p className='text-sm text-gray-100'>Back</p>
+              </button>
+            </Link>
+          }
+          right={
+            <button
+              type='button'
+              disabled={loading}
+              className={`${
+                loading
+                  ? 'bg-gray-200 text-gray-800 cursor-not-allowed'
+                  : 'text-red-500 hover:text-red-600 bg-red-50 hover:bg-red-100'
+              }  flex flex-row justify-between items-center py-2 px-3 rounded-md`}
+              onClick={deleteItem}
+            >
+              <HiOutlineTrash size='20' title='Delete this to-do item' />
+              <small className='ml-2'>Delete</small>
+            </button>
+          }
+        />
         <Wrapper className='p-4'>
           <div className='container'>
             <div className='max-w-xl mx-auto'>
-              <h5>Title: {itemTitleCapitalized}</h5>
+              <form onSubmit={handleSubmit}>
+                <label className='block mb-8' htmlFor='title'>
+                  <span className='text-gray-700'>Title</span>
+                  <input
+                    className='mt-1 w-full block rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0'
+                    type='text'
+                    name='title'
+                    required
+                    disabled={loading}
+                    placeholder={title}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </label>
+                <label className='block my-8' htmlFor='description'>
+                  <div className='flex flex-row items-center justify-between'>
+                    <span className='text-gray-700'>Description</span>
+                    <em className='text-gray-700 text-sm'>Optional</em>
+                  </div>
+                  <textarea
+                    className='mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0'
+                    name='description'
+                    rows='3'
+                    disabled={loading}
+                    placeholder={description}
+                    value={description}
+                    onChange={(e) => {
+                      setDescription(e.target.value);
+                    }}
+                  ></textarea>
+                </label>
+                <div className='mt-8'>
+                  <button
+                    className={`${
+                      loading
+                        ? 'bg-gray-200 cursor-not-allowed'
+                        : 'bg-green-500 hover:bg-green-600 text-white'
+                    } font-semibold tracking-wider py-3 px-8 rounded-lg w-full`}
+                    disabled={loading}
+                  >
+                    {loading ? 'Saving...' : 'Save'}
+                  </button>
+                </div>
+              </form>
+              {submitted && <Banner text='This to-do has been updated' />}
             </div>
           </div>
         </Wrapper>
