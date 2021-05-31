ee273fb4830af449ba27e6ff8bf49057e4186667
Ahmad Ihsan <ihsanashi@gmail.com>
2021-05-31 18:59:44 +0800

    make header component to render dynamic icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,16 @@
-import { HiOutlineUser, HiOutlineSearch } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 import Wrapper from './Wrapper';
 
-function Header() {
+function Header({ left, right }) {
   return (
     <Wrapper className='p-4 bg-indigo-500'>
       <div className='container'>
         <header className='max-w-xl mx-auto flex flex-row justify-between items-center'>
-          <button className='rounded-full p-2'>
-            <HiOutlineUser
-              className='text-gray-200'
-              size='22'
-              title='Profile icon'
-            />
-          </button>
-          <h6 className='text-lg font-semibold text-white'>Tudu</h6>
-          <button className='rounded-lg p-2'>
-            <HiOutlineSearch
-              className='text-gray-200'
-              size='20'
-              title='Search icon'
-            />
-          </button>
+          {left}
+          <Link to='/' className='text-lg font-semibold text-white'>
+            Tudu
+          </Link>
+          {right}
         </header>
       </div>
     </Wrapper>
