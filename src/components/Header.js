ac812cdb20ba1a7184014da821f3937d91a7b5c0
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-01 21:29:45 +0800

    remove dynamic icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import Wrapper from './Wrapper';
 
-function Header({ left, right }) {
+function Header() {
   return (
     <Wrapper className='p-4 bg-indigo-500'>
       <div className='container'>
-        <header className='max-w-xl mx-auto flex flex-row justify-between items-center'>
-          {left}
+        <header className='max-w-xl mx-auto flex flex-row justify-center items-center'>
           <Link to='/' className='text-lg font-semibold text-white'>
             Tudu
           </Link>
-          {right}
         </header>
       </div>
     </Wrapper>
