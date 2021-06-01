6b1bfe0017bbe46a3d34836438e1e493f8f34ed9
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-01 21:32:48 +0800

    remove dynamic Header elements, move add new and reset buttons

--- src/components/List.js
+++ src/components/List.js
@@ -7,57 +7,87 @@ function List() {
   const { todos, dispatch } = useContext(TodoContext);
 
   return (
-    <ul className='max-w-xl mx-auto'>
-      {todos.map((item) => (
-        <li
-          key={item.id}
-          className='flex flex-row items-start justify-between my-3 bg-gray-100 p-5 rounded-lg'
+    <div className='max-w-xl mx-auto'>
+      <div className='mb-5'>
+        <input
+          className='mt-0 block text-sm md:text-base mx-auto w-1/2 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black'
+          type='text'
+          placeholder='Search'
+          onChange={(e) =>
+            dispatch({
+              type: ACTIONS.FILTER,
+              payload: {
+                title: e.target.value,
+              },
+            })
+          }
+        />
+      </div>
+      <ul>
+        {todos.map((item) => (
+          <li
+            key={item.id}
+            className='flex flex-row items-start justify-between my-3 bg-gray-100 p-5 rounded-lg'
+          >
+            <div className='flex flex-row items-start justify-start'>
+              <input
+                className='rounded bg-gray-300 border-transparent focus:border-transparent focus:bg-gray-300 text-blue-700 focus:ring-1 focus:ring-offset-2 focus:ring-blue-500'
+                type='checkbox'
+                defaultChecked={item.complete}
+                onClick={() =>
+                  dispatch({
+                    type: ACTIONS.TOGGLE,
+                    payload: {
+                      id: item.id,
+                    },
+                  })
+                }
+              />
+              <p className='text-gray-800 text-base text-left ml-4'>
+                {item.title}
+              </p>
+            </div>
+            <div className='flex flex-row items-center'>
+              <Link
+                to={`/todo/${item.id}`}
+                id={item.id}
+                className='mr-3 text-sm text-blue-500 font-medium'
+                htmlFor={item.title}
+              >
+                View
+              </Link>
+              <button
+                type='button'
+                className='text-red-500 text-sm font-medium'
+                onClick={() =>
+                  dispatch({
+                    type: ACTIONS.REMOVE,
+                    payload: {
+                      id: item.id,
+                    },
+                  })
+                }
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className='flex flex-col items-center justify-center my-5'>
+        <Link to='/new' className='w-full flex items-center justify-center'>
+          <button className='bg-green-500 hover:bg-green-600 py-3 px-8 rounded-lg w-full shadow-md hover:shadow-lg text-base font-medium text-white capitalize tracking-wider'>
+            Add a new item
+          </button>
+        </Link>
+        <button
+          className='text-red-400 bg-red-50 hover:text-red-500 hover:bg-red-100 rounded-lg w-full py-3 mt-3'
+          onClick={() => dispatch({ type: ACTIONS.RESET })}
         >
-          <div className='flex flex-row items-start justify-start'>
-            <input
-              className='rounded bg-gray-300 border-transparent focus:border-transparent focus:bg-gray-300 text-blue-700 focus:ring-1 focus:ring-offset-2 focus:ring-blue-500'
-              type='checkbox'
-              defaultChecked={item.complete}
-              onClick={() =>
-                dispatch({
-                  type: ACTIONS.TOGGLE,
-                  payload: {
-                    id: item.id,
-                  },
-                })
-              }
-            />
-            <p className='text-gray-800 text-base text-left ml-4'>
-              {item.title}
-            </p>
-          </div>
-          <div className='flex flex-row items-center'>
-            <Link
-              to={`/todo/${item.id}`}
-              id={item.id}
-              className='mr-3 text-sm text-blue-500 font-medium'
-              htmlFor={item.title}
-            >
-              View
-            </Link>
-            <button
-              type='button'
-              className='text-red-500 text-sm font-medium'
-              onClick={() =>
-                dispatch({
-                  type: ACTIONS.REMOVE,
-                  payload: {
-                    id: item.id,
-                  },
-                })
-              }
-            >
-              Delete
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
+          Reset
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import { Helmet } from 'react-helmet';
 import Header from '../components/Header';
-import { HiOutlineUser } from 'react-icons/hi';
 import { TodoContext } from '../contexts/TodoContext';
 import { useContext } from 'react';
 import Wrapper from '../components/Wrapper';
 import { Link } from 'react-router-dom';
 import List from '../components/List';
-import { ACTIONS } from '../reducers/TodoReducer';
 
 function Home() {
-  const { todos, dispatch } = useContext(TodoContext);
+  const { todos } = useContext(TodoContext);
 
   return (
     <>
@@ -20,25 +18,8 @@ function Home() {
         />
         <title>Tudu - Minimal To-do List</title>
       </Helmet>
-      <Header
-        left={
-          <button className='rounded-full p-2'>
-            <HiOutlineUser
-              className='text-gray-200'
-              size='22'
-              title='Profile icon'
-            />
-          </button>
-        }
-        right={
-          <input
-            type='text'
-            className='mt-0 block bg-indigo-500 px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-white text-sm text-white placeholder-gray-300'
-            placeholder='Search...'
-          />
-        }
-      />
-      <Wrapper className='p-4 my-8 text-center'>
+      <Header />
+      <Wrapper className='p-4 my-5 text-center'>
         <div className='container'>
           {todos.length === 0 ? (
             <div className='max-w-xl mx-auto'>
@@ -54,24 +35,6 @@ function Home() {
             </div>
           ) : (
             <List />
-          )}
-          {todos.length !== 0 && (
-            <div className='max-w-xl mx-auto flex flex-col items-center justify-center my-5'>
-              <Link
-                to='/new'
-                className='w-full flex items-center justify-center'
-              >
-                <button className='bg-green-500 hover:bg-green-600 py-3 px-8 rounded-lg w-full shadow-md hover:shadow-lg text-base font-medium text-white capitalize tracking-wider'>
-                  Add a new item
-                </button>
-              </Link>
-              <button
-                className='text-red-400 bg-red-50 hover:text-red-500 hover:bg-red-100 rounded-lg w-full py-3 mt-3'
-                onClick={() => dispatch({ type: ACTIONS.RESET })}
-              >
-                Reset
-              </button>
-            </div>
           )}
         </div>
       </Wrapper>
