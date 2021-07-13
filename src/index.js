09174dd80cba3a6ec77da34046e7cdeb7b3c8ff8
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-13 17:29:05 +0800

    initialise auth0 in the app root

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />,
+  </Auth0Provider>,
+  document.getElementById('root')
+);
