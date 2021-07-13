6819f6d3692a006bf4fe085e60287d384cf007b7
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-13 17:28:05 +0800

    add the routes back in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import routes from './config/routes';
+import Home from './pages/index';
+import AddNew from './pages/new';
+import Landing from './pages/Landing';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+import ResetPassword from './pages/auth/reset';
+import ResetPasswordStatus from './pages/auth/resetStatus';
+import Help from './pages/Help';
+import ViewTodo from './pages/View';
+import NoMatch from './pages/NoMatch';
 
 import { TodoProvider } from './contexts/TodoContext';
 
@@ -15,15 +23,24 @@ function App() {
       <TodoProvider>
         <Router>
           <Switch>
-            {routes.map((route) => (
-              <Route
-                exact
-                strict
-                key={route.path}
-                path={route.path}
-                component={route.component}
-              />
-            ))}
+            <Route exact path='/' component={Home} />
+            <Route exact path='/new' component={AddNew} />
+            <Route exact path='/landing' component={Landing} />
+            <Route exact path='/auth/login' component={Login} />
+            <Route exact path='/auth/register' component={Register} />
+            <Route
+              exact
+              path='/auth/reset-password'
+              component={ResetPassword}
+            />
+            <Route
+              exact
+              path='/auth/reset-password-status'
+              component={ResetPasswordStatus}
+            />
+            <Route exact path='/help' component={Help} />
+            <Route exact strict path='/todo/:id' component={ViewTodo} />
+            <Route path='*' component={NoMatch} />
           </Switch>
         </Router>
       </TodoProvider>
