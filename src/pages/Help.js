b7403c0f6f19480974b1b574aa6c2f2629e5c13c
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-14 16:45:10 +0800

    turn help page into under construction page

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet';
 import Wrapper from '../components/Wrapper';
+import { Link } from 'react-router-dom';
 
 function Help() {
   return (
@@ -8,9 +9,21 @@ function Help() {
         <title>Tudu - Help</title>
         <meta name='description' content='Get help using Tudu here.' />
       </Helmet>
-      <Wrapper className='p-4 my-5'>
+      <Wrapper className='p-4'>
         <div className='container'>
-          <h2>Get help</h2>
+          <div className='max-w-xl mx-auto'>
+            <div className='py-4'>
+              <h5 className='mb-3 text-lg font-medium md:text-xl'>
+                Page under maintenance
+              </h5>
+              <Link
+                to='/'
+                className='inline-block font-medium text-gray-800 border-b border-black hover:text-blue-500 hover:border-blue-500'
+              >
+                Return to homepage
+              </Link>
+            </div>
+          </div>
         </div>
       </Wrapper>
     </>
