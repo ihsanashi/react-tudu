1e3a6c6912e7396c47da0fa9d59d1a1da9a0d55a
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-15 13:02:56 +0800

    remove help link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,23 +6,21 @@ function Header() {
   const { logout } = useAuth0();
 
   return (
-    <Wrapper className='p-4 bg-indigo-500'>
+    <Wrapper className='p-4 bg-indigo-200 shadow-sm'>
       <div className='container'>
         <header className='flex flex-row items-center justify-between max-w-xl mx-auto'>
+          <Link
+            to='/'
+            className='inline-block text-lg font-semibold text-black transition duration-300 ease-in-out border-b border-transparent hover:border-black'
+          >
+            Tudu
+          </Link>
           <button
-            className='px-2 py-1 text-sm font-medium text-gray-100 transition duration-300 ease-in-out rounded-md hover:text-white hover:bg-red-500'
+            className='px-2 py-1 text-sm font-medium text-gray-600 transition duration-300 ease-in-out rounded-md hover:text-white hover:bg-red-500'
             onClick={() => logout({ returnTo: window.location.origin })}
           >
             Log Out
           </button>
-          <Link to='/' className='text-lg font-semibold text-white'>
-            Tudu
-          </Link>
-          <Link to='/help'>
-            <p className='text-sm font-normal text-gray-200 transition duration-300 ease-in-out border-b border-transparent hover:text-white hover:border-white'>
-              Help?
-            </p>
-          </Link>
         </header>
       </div>
     </Wrapper>
