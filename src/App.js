60fa4c620808f1657f522ee2ec3f5d32fec61c6c
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-15 13:04:24 +0800

    remove Help page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/index';
 import AddNew from './pages/new';
 import Landing from './pages/Landing';
-import Help from './pages/Help';
 import ViewTodo from './pages/View';
 import NoMatch from './pages/NoMatch';
 import ProtectedRoute from './auth/protected-route';
@@ -29,7 +28,6 @@ function App() {
               path='/todo/:id'
               component={ViewTodo}
             />
-            <ProtectedRoute exact path='/help' component={Help} />
             <Route exact path='/landing' component={Landing} />
             <Route path='*' component={NoMatch} />
           </Switch>
