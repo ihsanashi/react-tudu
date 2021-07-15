548b5400b2b20b1dbd030a6338762b64177c1148
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-15 13:04:41 +0800

    update styles

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -16,16 +16,16 @@ function NoMatch() {
         <div className='container'>
           <div className='max-w-xl mx-auto'>
             <div className='py-4'>
-              <h1 className='text-2xl font-semibold mb-4'>
+              <h1 className='mb-4 text-2xl font-semibold'>
                 404 Error, page not found
               </h1>
-              <h3 className='text-gray-700 text-lg font-normal'>
+              <h3 className='text-lg font-normal text-gray-700'>
                 No match for <code>{location.pathname}</code>
               </h3>
               <div className='mt-5'>
                 <Link
                   to='/'
-                  className='font-medium text-gray-800 hover:text-blue-500 border-b border-black hover:border-blue-500'
+                  className='font-medium text-gray-800 border-b border-black hover:text-indigo-500 hover:border-indigo-500'
                 >
                   Return to homepage
                 </Link>
