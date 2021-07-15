c42eefbe4ea63669a73ae3d02d2c9222ec1d5b7f
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-15 13:03:50 +0800

    persist login

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ ReactDOM.render(
     domain={domain}
     clientId={clientId}
     redirectUri={window.location.origin}
+    useRefreshTokens={true}
+    cacheLocation='localstorage'
   >
     <App />
   </Auth0Provider>,
