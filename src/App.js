77e334c5b2fb0f6c6e4b041ddc6e2e7fa7d45026
Ahmad Ihsan <ihsanashi@gmail.com>
2021-08-23 17:18:36 +0800

    remove TodoProvider component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/index';
 import AddNew from './pages/new';
 import Landing from './pages/Landing';
-import ViewTodo from './pages/View';
 import NoMatch from './pages/NoMatch';
 import ProtectedRoute from './auth/protected-route';
 import { useAuth0 } from '@auth0/auth0-react';
 
-import { TodoProvider } from './contexts/TodoContext';
 import Loading from './components/Loading';
 
 function App() {
@@ -16,24 +14,15 @@ function App() {
   if (isLoading) return <Loading />;
 
   return (
-    <>
-      <TodoProvider>
-        <Router>
-          <Switch>
-            <ProtectedRoute exact path='/' component={Home} />
-            <ProtectedRoute exact path='/new' component={AddNew} />
-            <ProtectedRoute
-              exact
-              strict
-              path='/todo/:id'
-              component={ViewTodo}
-            />
-            <Route exact path='/landing' component={Landing} />
-            <Route path='*' component={NoMatch} />
-          </Switch>
-        </Router>
-      </TodoProvider>
-    </>
+    <Router>
+      <Switch>
+        <ProtectedRoute exact path='/' component={Home} />
+        <ProtectedRoute exact path='/new' component={AddNew} />
+        {/* <ProtectedRoute exact strict path='/todo/:id' component={ViewTodo} /> */}
+        <Route exact path='/landing' component={Landing} />
+        <Route path='*' component={NoMatch} />
+      </Switch>
+    </Router>
   );
 }
 
